d3e68db1ae9bffc1914b4ce60c6e9574bcaa538d
OHMYEONGBIN <64066925+OHMYEONGBIN@users.noreply.github.com>
2024-06-04 05:15:45 +0900

    mobility 추가

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const path = require('path');
-const mysql = require('mysql2');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// MySQL connection
+
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'mysql', 
-    database: 'shared_mobility' 
-  });
-  
-  db.connect((err) => {
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'sj19981220',
+  database: 'Shared_mobility'
+});
+
+db.connect((err) => {
     if (err) {
-      throw err;
+        throw err;
     }
     console.log('MySQL Connected...');
-  });
+});
 
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -27,38 +27,39 @@ app.use(express.static(path.join(__dirname, 'views')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Body parser middleware
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
-
-// Use routes from routes/user.js
+// Routes
+const mobilityRoutes = require('./routes/mobility');
+const stationRoutes = require('./routes/station');
 const userRoutes = require('./routes/user');
-app.use('/api/users', userRoutes(db)); // Pass db connection to routes
+
+// Use routes
+app.use('/api/mobility', mobilityRoutes(db));
+app.use('/api/stations', stationRoutes(db));
+app.use('/api/users', userRoutes(db));
 
 // Serve HTML files
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'home.html'));
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
 
 app.get('/user.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'user.html'));
+    res.sendFile(path.join(__dirname, 'views', 'user.html'));
 });
 
 app.get('/edit_user.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'edit_user.html'));
-  });
+});
 
-// More HTML routes as needed
 app.get('/contact.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+    res.sendFile(path.join(__dirname, 'views', 'contact.html'));
 });
 
 app.get('/mobility.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'mobility.html'));
+    res.sendFile(path.join(__dirname, 'views', 'mobility.html'));
 });
 
 app.get('/station.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'station.html'));
+    res.sendFile(path.join(__dirname, 'views', 'station.html'));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
